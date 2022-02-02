4802ca7d6080bd484333e7795f5e70485103ebeb
somya1209 <somya123024@gmail.com>
2022-02-02 23:05:19 +0530

    adding swagger dependency

--- src/main/java/com/twine/businessinfoservice/BusinessInfoServiceApplication.java
+++ src/main/java/com/twine/businessinfoservice/BusinessInfoServiceApplication.java
@@ -3,17 +3,21 @@ package com.twine.businessinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
+@EnableSwagger2
 @SpringBootApplication
 public class BusinessInfoServiceApplication {
-//    @Bean
-//    public Docket docket(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.twine.businessinfoservice"))
-//                .build();
-//    }
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.twine.businessinfoservice"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BusinessInfoServiceApplication.class, args);
